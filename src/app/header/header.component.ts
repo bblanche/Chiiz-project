164626fe5f1b178b3fceaf19b735d321d922af9c
Fred Ouamba <harissonfred2@gmail.com>
2024-12-20 11:00:13 +0100

    connection des boutons du header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,4 +12,8 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  scrollToBottom(): void {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+  }
+
 }
